846166468132c05f88c14b5e70d197880e848646
Rishi <rishikarir0@example.com>
2025-12-05 22:03:06 +0530

    committed

--- src/Generating.jsx
+++ src/Generating.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import { generateMealPlan } from './services/geminiservices.js';
+import { generateMealPlan } from '/src/services/geminiservices.js';
 
 
 
@@ -11,7 +11,6 @@ export default function Generating(){
 const lines = ["Analyzing height/weight...",
 "Understanding your diet...",
 "Designing your diet..."]
-
 
 
 
